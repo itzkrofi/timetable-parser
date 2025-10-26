2832faa37df27c2ddb6d6a59b528981db220735c
itzkrofi <kruszeleq@gmail.com>
2025-10-26 11:50:54 +0100

    lol

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import Table from "./table.js";
-import TimeTableList from "./timeTableList.js";
-import TimeTable from "./timeTable.js";
-export { Table, TimeTableList, TimeTable };
-export * from "./types.js";
