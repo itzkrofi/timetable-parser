317270e86da571a4588c3a44b109c95c944a6d75
itzkrofi <kruszeleq@gmail.com>
2025-10-26 11:45:17 +0100

    fix: removed ts

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import Table from "./table.js";
+import TimeTableList from "./timeTableList.js";
+import TimeTable from "./timeTable.js";
+export { Table, TimeTableList, TimeTable };
+export * from "./types.js";
